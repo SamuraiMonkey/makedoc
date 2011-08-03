9b7179b02f3ccebf628dd254bd139b5e10f1cdd4
Christina Maxwell <christina.em7@gmail.com>
2011-08-03 09:59:11 +0200

    Initial commit.

---
+++ makedoc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import pydoc
+import sys
+import imp
+
+if len(sys.argv) < 3 :
+    print 'Command incorrect. Pls use this format:'
+    print 'python makedoc.py FileNameToWriteTo Module'
+else:
+    fname = sys.argv[1]
+    module = sys.argv[2]
+    __import__(module)
+
+    fobj = open(fname, 'a')
+    fobj.write(pydoc.render_doc(module))
+    fobj.close()
+    print '%s has been updated with the docstrings from %s.' % (fname, module)
